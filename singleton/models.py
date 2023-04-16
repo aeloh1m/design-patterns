3d5f8a2c60bb6565784514e9d6b856b8c52bb1ee
aeloh1m <melowgz@gmail.com>
2023-04-16 18:59:00 -0300

    finished singleton in admin page

--- singleton/models.py
+++ singleton/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from django.db import models
+
 # Create your models here.
+
+
+class SingletonModel (models.Model):
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+
+        super().save(*args, **kwargs)
+
+
+class Settings(SingletonModel):
+
+    hours = models.IntegerField()
+    days = models.CharField(max_length=30)
