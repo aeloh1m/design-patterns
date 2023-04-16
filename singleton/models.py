073e0b45fc47a50eaebc89c27485487ae4ff0f2f
aeloh1m <melowgz@gmail.com>
2023-04-16 19:07:53 -0300

    singleton functional

--- singleton/models.py
+++ singleton/models.py
@@ -5,13 +5,16 @@ from django.db import models
 # Create your models here.
 
 
+# involves a single class which is responsible to create an object while making sure that only single object gets created
+
 class SingletonModel (models.Model):
 
     def save(self, *args, **kwargs):
-        self.pk = 1
+        self.pk = 1 # Indicates that the field will be always replaced by the same id (1)
 
         super().save(*args, **kwargs)
 
+# Values for the database
 
 class Settings(SingletonModel):
 
